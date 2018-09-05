8a502f9725a3f3e17213ce4741e35263146ff2d7
torbjorn@angeltveit.info <angeltveit>
2018-09-05 11:52:20 +0200

    made a tiny generator

---
+++ gulp/new-component.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp'),
+    lodash = require('lodash'),
+    rename = require('gulp-rename'),
+    conflict = require('gulp-conflict'),
+    template = require('gulp-template'),
+    handlebars = require('handlebars'),
+    fs = require('fs'),
+    pluralize = require('pluralize');
+
+gulp.task('create-component', function (done) {
+  var answers = {component: process.argv.slice(2)[1]}
+  answers.property = lodash.camelCase(answers.component)
+  answers.component = lodash.kebabCase(answers.component)
+  answers.className = lodash.capitalize(answers.property)
+  answers.endpoint = pluralize(lodash.snakeCase(answers.property))
+  if(!fs.existsSync(`${__dirname}/../src/components/${answers.component}`)) {
+    fs.mkdirSync(`${__dirname}/../src/components/${answers.component}`)
+  }
+  const file = fs.readFileSync(`${__dirname}/../helpers/templates/component.js`, 'utf-8')
+  fs.writeFileSync(`${__dirname}/../src/components/${answers.component}/${answers.component}.js`, handlebars.compile(file)(answers))
+})
--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,2 @@
 require('./gulp/build')
+require('./gulp/new-component')
